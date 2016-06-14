c7ce976e44d77bd123685447dfe3e9fd2abb2af3
Shigeki Suwa <suwa.shigeki@gmail.com>
2016-06-14 18:25:10 +0900

    gulp

--- README.md
+++ README.md
@@ -1,5 +1,40 @@
+# 環境構築
+
+- npmをインストール(無ければ)
+```sh
+$ brew install npm
+```
+
+- gulpをインストール(無ければ)
+```sh
+$ npm install -g gulp
+```
+
+- プロージェクトルートで
+```sh
+$ npm install
+```
+
+---
+
+# 開発時に使うコマンド
+- ソースファイルをコンパイル
+  - 全部一回コンパイル
+  ```sh
+  $ gulp
+  ```
+
+  - ファイルの変更を監視して自動コンパイル
+  ```sh
+  $ gulp watch
+  ```
+
+---
+
 # APIs
 
 - `GET` `/api/student/list.json`
 - `POST` `/api/student/update.json`
 - `GET` `/api/location/list.json`
+
+- APIドキュメントあるらしい
---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var nop = require('gulp-nop');
+
+var tasks = {
+  sass: { // can wrap array
+    src: 'assets/sass/**/*.scss', // can wrap array
+    dest: 'public/css/'
+  },
+  js: {
+    src: 'assets/js/**/*.js',
+    dest: 'public/js/',
+    concat: false,
+    destfile: 'app.js'
+  }
+};
+
+gulp.task('sass', function() {
+  if( !tasks.sass ) return;
+
+  (Array.isArray(tasks.sass) ? tasks.sass : [tasks.sass]).forEach(function(task) {
+    gulp.src(task.src)
+    .pipe(sass())
+    .pipe(gulp.dest(task.dest));
+  });
+});
+
+gulp.task('js', function() {
+  if( !tasks.js ) return;
+
+  (Array.isArray(tasks.js) ? tasks.js : [tasks.js]).forEach(function(task) {
+    gulp.src(task.src)
+    .pipe(uglify())
+    .pipe(task.concat ? concat(task.destfile) : nop())
+    .pipe(gulp.dest(task.dest))
+  });
+});
+
+gulp.task('default', [
+  'sass',
+  'js'
+]);
+
+gulp.task('watch', ['default'], function() {
+  for( prop in tasks ) {
+    (Array.isArray(tasks[prop]) ? tasks[prop] : [tasks[prop]]).forEach(function(task) {
+      gulp.watch(task.src, [prop]);
+    });
+  }
+});
