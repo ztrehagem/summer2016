191eeb6595cc3a119d6dd55f9a90806cb423e7ba
Shigeki Suwa <suwa.shigeki@gmail.com>
2016-06-15 11:05:52 +0900

    canvas

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,7 +1,40 @@
 angular.module('app')
 
-.run(['apis', function(apis) {
-  var that = this;
+.run(['$window', '$document', 'apis', function($window, $document, apis) {
+  console.log('app run');
 
-  console.log('it works!');
+  var that = this;
+  var canvas = $document.find('canvas')[0];
+  var ctx = canvas.getContext('2d');
+  var requestAnimationFrame = $window.requestAnimationFrame || $window.mozRequestAnimationFrame || $window.webkitRequestAnimationFrame || $window.msRequestAnimationFrame;
+
+  setCanvasWidth();
+  $window.addEventListener('resize', setCanvasWidth);
+
+  var x = 0, y = 0;
+  // draw();
+
+  // --- functions --- //
+
+  function draw() {
+    x++;
+    y++;
+
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = '#fff';
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, 2 * Math.PI, false);
+    ctx.closePath();
+    ctx.fill();
+
+    requestAnimationFrame(draw);
+  }
+
+  function setCanvasWidth() {
+    canvas.width = $window.innerWidth;
+    canvas.height = $window.innerHeight;
+    console.log('canvas resized');
+  }
 }]);
